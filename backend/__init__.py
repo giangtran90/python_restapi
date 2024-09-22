f50395a39043518ab953b378e233a26dd6deccf7
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-09-22 12:20:50 +0900

    cach cau hinh va them file config cao du an

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, Blueprint
 
-def create_app():
+# config_file = 'config.py' giup load file config len
+def create_app(config_file = 'config.py'):
     app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    print(app.config['SECRET_KEY'])
     return app
