a94897a54ed5ef0c61588f736fa9eb1e58ba96ab
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-09-23 20:42:15 +0900

    thuc hien api GET, POST, PUT, DELETE

--- backend/books/controller.py
+++ backend/books/controller.py
@@ -1,14 +1,29 @@
 from flask import Blueprint
-from .services import add_book_service
+from .services import add_book_service, get_book_by_id_service, get_all_books_service, update_book_service, delete_book_by_id_service
 
 books = Blueprint('books', __name__)
-
-
-@books.route('/get-all-books')
-def get_all_books():
-    return 'get all books'
 
 # add a new book
 @books.route('/book-management/book', methods=['POST'])
 def add_book():
     return add_book_service()
+
+# get book by id
+@books.route('/book-management/book/<int:id>', methods=['GET'])
+def get_book(id):
+    return get_book_by_id_service(id)
+
+# get all books
+@books.route('/book-management/books', methods=['GET'])
+def get_all_books():
+    return get_all_books_service()
+
+# update book by id
+@books.route('/book-management/book/<int:id>', methods=["PUT"])
+def update_book_by_id(id):
+    return update_book_service(id)
+
+# delete book by id
+@books.route('/book-management/book/<int:id>', methods=['DELETE'])
+def delete_book_by_id(id):
+    return delete_book_by_id_service(id)
--- backend/books/services.py
+++ backend/books/services.py
@@ -1,25 +1,70 @@
 from backend.extension import db
 from backend.library_ma import BookSchema
 from backend.model import Books
-from flask import request
+from flask import request, jsonify
 import json
 
 # lay ra 1 quyen sach
-book_schema = BookSchema
+book_schema = BookSchema()
 # lay ra tat ca quyen sach
 books_schema = BookSchema(many=True)
 
 def add_book_service():
-    name = request.json['name']
-    page_count = request.json['page_count']
-    author_id = request.json['author_id']
-    category_id = request.json['category_id']
+    data = request.json
+    if data and ('name' in data) and ('page_count' in data) and ('author_id' in data) and ('category_id' in data):
+        name = request.json['name']
+        page_count = request.json['page_count']
+        author_id = request.json['author_id']
+        category_id = request.json['category_id']
 
-    try:
-        new_book = Books(name,page_count,author_id,category_id)
-        db.session.add(new_book)
-        db.session.commit()
-        return "Add success"
-    except IndentationError:
-        db.session.rollback()
-        return "Can not add book"
+        try:
+            new_book = Books(name,page_count,author_id,category_id)
+            db.session.add(new_book)
+            db.session.commit()
+            return "Add success"
+        except IndentationError:
+            db.session.rollback()
+            return "Can not add book"
+    else:
+        return "Request error!!!"
+    
+def get_book_by_id_service(id):
+    book = Books.query.get(id)
+    if book:
+        return book_schema.jsonify(book)
+    else:
+        return "Not found"
+    
+def get_all_books_service():
+    books = Books.query.all()
+    if books:
+        return books_schema.jsonify(books)
+    else:
+        return "Not found"
+    
+def update_book_service(id):
+    book = Books.query.get(id)
+    data = request.json
+    if book and 'page_count' in data:
+        try:           
+            book.page_count = data['page_count']
+            db.session.commit()
+            return "Book Updated"
+        except IndentationError:
+            db.session.rollback()
+            return "Can not update book"
+    else:
+        return "Not found book"
+
+def delete_book_by_id_service(id):
+    book = Books.query.get(id)
+    if book:
+        try:
+            db.session.delete(book)
+            db.session.commit()
+            return "Book Deleted"
+        except IndentationError:
+            db.session.rollback()
+            return "Can not delete book"
+    else:
+        return "Not found book"
